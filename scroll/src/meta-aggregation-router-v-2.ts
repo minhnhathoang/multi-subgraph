592bbd8dba8ee66a93b40d872cb85f430da879d8
nhathm <nhathm.uet@outlook.com>
2024-09-09 01:50:52 +0700

    Initialize subgraph

--- scroll/src/meta-aggregation-router-v-2.ts
+++ scroll/src/meta-aggregation-router-v-2.ts
@@ -14,16 +14,24 @@ import {
   OwnershipTransferred,
   Swapped
 } from "../generated/schema"
+import {Address, Bytes} from "@graphprotocol/graph-ts";
+
 
 export function handleClientData(event: ClientDataEvent): void {
   let entity = new ClientData(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.clientData = event.params.clientData
+  entity.address = event.address
 
   entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
+  entity.blockHash = event.block.hash
+
   entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
+  entity.clientData = event.params.clientData
 
   entity.save()
 }
@@ -32,11 +40,17 @@ export function handleError(event: ErrorEvent): void {
   let entity = new Error(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.reason = event.params.reason
+  entity.address = event.address
 
   entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
+  entity.blockHash = event.block.hash
+
   entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
+  entity.reason = event.params.reason
 
   entity.save()
 }
@@ -45,31 +59,49 @@ export function handleExchange(event: ExchangeEvent): void {
   let entity = new Exchange(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+
+  entity.address = event.address
+
+  entity.blockNumber = event.block.number
+  entity.blockHash = event.block.hash
+
+  entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
   entity.pair = event.params.pair
   entity.amountOut = event.params.amountOut
   entity.output = event.params.output
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
 
 export function handleFee(event: FeeEvent): void {
-  let entity = new Fee(event.transaction.hash.concatI32(event.logIndex.toI32()))
-  entity.token = event.params.token
-  entity.totalAmount = event.params.totalAmount
-  entity.totalFee = event.params.totalFee
-  entity.recipients = event.params.recipients
-  entity.amounts = event.params.amounts
-  entity.isBps = event.params.isBps
+  let entity = new Fee(event.transaction.hash.concatI32(event.logIndex.toI32()));
+
+  entity.address = event.address
 
   entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
+  entity.blockHash = event.block.hash
 
-  entity.save()
+  entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
+  entity.token = event.params.token;
+  entity.totalAmount = event.params.totalAmount;
+  entity.totalFee = event.params.totalFee;
+
+  entity.recipients = event.params.recipients.map<Bytes>((recipient: Address): Bytes => {
+    return recipient as Bytes;
+  });
+
+  entity.amounts = event.params.amounts;
+  entity.isBps = event.params.isBps;
+
+  entity.save();
 }
 
 export function handleOwnershipTransferred(
@@ -78,12 +110,19 @@ export function handleOwnershipTransferred(
   let entity = new OwnershipTransferred(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
-  entity.previousOwner = event.params.previousOwner
-  entity.newOwner = event.params.newOwner
+
+  entity.address = event.address
 
   entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
+  entity.blockHash = event.block.hash
+
   entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
 
   entity.save()
 }
@@ -92,16 +131,22 @@ export function handleSwapped(event: SwappedEvent): void {
   let entity = new Swapped(
     event.transaction.hash.concatI32(event.logIndex.toI32())
   )
+  entity.address = event.address
+
+  entity.blockNumber = event.block.number
+  entity.blockHash = event.block.hash
+
+  entity.transactionHash = event.transaction.hash
+  entity.transactionIndex = event.transaction.index
+
+  entity.logIndex = event.logIndex
+
   entity.sender = event.params.sender
   entity.srcToken = event.params.srcToken
   entity.dstToken = event.params.dstToken
   entity.dstReceiver = event.params.dstReceiver
   entity.spentAmount = event.params.spentAmount
   entity.returnAmount = event.params.returnAmount
-
-  entity.blockNumber = event.block.number
-  entity.blockTimestamp = event.block.timestamp
-  entity.transactionHash = event.transaction.hash
 
   entity.save()
 }
